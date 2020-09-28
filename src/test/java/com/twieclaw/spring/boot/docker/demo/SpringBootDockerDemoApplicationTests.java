f15a4ead3dcd9713cd91444c1005dce9803e0631
tomaszwieclaw <tomash.oov@gmail.com>
2020-09-28 17:00:47 +0200

    Init spring project.

---
+++ src/test/java/com/twieclaw/spring/boot/docker/demo/SpringBootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twieclaw.spring.boot.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
